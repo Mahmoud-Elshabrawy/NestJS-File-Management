50d12053b8f86aae6bc646f925b3f879c85231d4
Mahmoud Elshabrawy <mahmoudwaell391@gmail.com>
2026-02-04 01:49:37 +0200

    use built-in file pipe validators

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, FileTypeValidator, HttpStatus, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { error } from 'console';
 
 @Controller('file-upload')
 export class FileUploadController {
     @Post('/single')
-    @UseInterceptors(FileInterceptor('file', {
-        limits: {
-            fileSize: 5 * 1024 * 1024 // 5MB
-        }
-    }))
-    uploadFile(@UploadedFile() file: Express.Multer.File) {
+    @UseInterceptors(FileInterceptor('file'))
+    uploadFile(@UploadedFile(new ParseFilePipe({
+        validators: [
+            // 1) Validate the file size
+            new MaxFileSizeValidator({
+                maxSize: 1024 * 1024, // 1MB
+                message: (maxSize) => `File is too big, max size is ${maxSize}`
+            }),
+            // 2) Validate the file type (extension)
+            new FileTypeValidator({
+                fileType: /png|jpg|jpeg/
+            })
+        ],
+        errorHttpStatusCode: HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+        exceptionFactory: (error: string) => {
+            throw new BadRequestException(error)
+        },
+        fileIsRequired: true //DEFAULT
+
+    })) file: Express.Multer.File) {
         return file
     }
 
@@ -19,7 +34,25 @@ export class FileUploadController {
             fileSize: 5 * 1024 * 1024 // 5MB
         }
     }))
-    uploadFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    uploadFiles(@UploadedFiles(new ParseFilePipe({
+        validators: [
+            // 1) Validate the file size
+            new MaxFileSizeValidator({
+                maxSize: 5 * 1024 * 1024, // 1MB
+                message: (maxSize) => `File is too big, max size is ${maxSize}`
+            }),
+            // 2) Validate the file type (extension)
+            new FileTypeValidator({
+                fileType: /png|jpg|jpeg/
+            })
+        ],
+        errorHttpStatusCode: HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+        exceptionFactory: (error: string) => {
+            throw new BadRequestException(error)
+        },
+        fileIsRequired: true //DEFAULT
+
+    })) files: Express.Multer.File[]) {
         return files.map((file) => file.originalname)
     }
 }
